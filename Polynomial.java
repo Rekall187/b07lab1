9800a89e77a3e6cc593b0e9586502401f29bc565
Rekall187 <144956011+Rekall187@users.noreply.github.com>
2023-09-25 21:08:40 -0400

    Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,24 +1,24 @@
 public class Polynomial{
     double[] coefficient;
     public Polynomial(){
-        coefficient = new double[4];
+        this.coefficient = new double[4];
     }
     public Polynomial(double [] a){
-        coefficient = a.clone();
+        this.coefficient = a.clone();
     }
     public Polynomial add (Polynomial x){
-        Polynomial A = new Polynomial(coefficient);
+        Polynomial A = new Polynomial(this.coefficient);
         int size = x.coefficient.length - 1;
         for (int i = 0; i < size; i++){
-            A.coefficient[i] = coefficient[i] + x.coefficient[i];
+            A.coefficient[i] = this.coefficient[i] + x.coefficient[i];
         }
         return A;
     }
     public double evaluate (double num){
         double temp = 0.0;
-        Polynomial axe = new Polynomial(coefficient);
+        //Polynomial axe = new Polynomial(this.coefficient);
         for (int i = 0; i< axe.coefficient.length; i++){
-            double hold = coefficient[i];
+            double hold = this.coefficient[i];
             for (int c = 0; c < i; c++){
                 hold = hold*num;
             }
